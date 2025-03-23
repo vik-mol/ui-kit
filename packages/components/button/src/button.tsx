c20241145d291669d96c58e96a5abc3883e3d1f1
Mol Victor <molvv@bcs.ru>
2025-03-23 22:58:18 +0400

    feat(button): change

--- packages/components/button/src/button.tsx
+++ packages/components/button/src/button.tsx
@@ -5,7 +5,7 @@ import { FC } from "react";
 import Style from './button.module.sass'
 
 const Button: FC = () => {
-  return <button className={Style.button} >button </button>;
+  return <button className={Style.button} >xxxx </button>;
 };
 
 export default Button;

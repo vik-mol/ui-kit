0765415be4f67006354f07ee86557c1d34bf5c29
Mol Victor <molvv@bcs.ru>
2025-03-23 22:16:47 +0400

    feat(@vik-mol/button): add component

---
+++ packages/components/button/src/button.stories.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Button from "./button";
+
+const meta = {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoggedOut: Story = {};

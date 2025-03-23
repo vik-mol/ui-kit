58c8af66f77b6a7153618d90b1f6e25518c743e6
vik-mol <56967918+vik-mol@users.noreply.github.com>
2025-03-23 22:19:18 +0400

    feat(@vik-mol/button): add component
    Co-authored-by: Mol Victor <molvv@bcs.ru>

---
+++ packages/components/button/src/button.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { FC } from "react";
+
+import Style from './button.module.sass'
+
+const Button: FC = () => {
+  return <button className={Style.button} >button </button>;
+};
+
+export default Button;

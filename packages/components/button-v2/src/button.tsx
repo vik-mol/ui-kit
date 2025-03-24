98456bb33d6729c19a25bac063f067f4d5999e68
Mol Victor <molvv@bcs.ru>
2025-03-24 16:33:32 +0400

    feat(@vik-mol/button-v2): change border color

---
+++ packages/components/button-v2/src/button.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { FC } from "react";
+
+import Style from './button.module.sass'
+
+const Button: FC = () => {
+  return <button className={Style.button} >xxxx </button>;
+};
+
+export default Button;

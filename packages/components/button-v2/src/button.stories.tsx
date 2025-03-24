ef001a21311ac386a2fb14594b4de7e043a01475
Mol Victor <molvv@bcs.ru>
2025-03-24 16:36:05 +0400

    feat(button-v2): nge border color

--- packages/components/button-v2/src/button.stories.tsx
+++ packages/components/button-v2/src/button.stories.tsx
@@ -5,7 +5,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Button from "./button";
 
 const meta = {
-  title: "Components/Button",
+  title: "Components/Button-v2",
   component: Button,
   tags: ["autodocs"],
 } satisfies Meta<typeof Button>;

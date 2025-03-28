c3a755e8526e378ad51b3259cfc302f527f0ae36
Mol Victor <molvv@bcs.ru>
2025-03-28 15:32:09 +0400

    fix: change storybook.yml

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,20 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+/** @format */
 
-import { join, dirname } from 'path';
+import type { StorybookConfig } from "@storybook/react-vite";
+
+import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
+  return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ['../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [getAbsolutePath('@storybook/addon-essentials')],
+  stories: ["../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
 };
